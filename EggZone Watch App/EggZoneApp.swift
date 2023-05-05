f05bbf2fb25978bc3aee81fa7e0efdfab9617fb9
Richard Hannagan <3982808+richardhannagan@users.noreply.github.com>
2023-05-05 14:47:49 +1000

    Initial Commit

---
+++ EggZone Watch App/EggZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggZoneApp.swift
+//  EggZone Watch App
+//
+//  Created by Richard Hannagan on 5/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct EggZone_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
